91cde6246437bb9df6261cadb679ca04e724185e
Mohammed Mansoor <mansoor6328@gmail.com>
2022-01-15 18:50:06 +0530

    commit eros now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
-import { BrowserRouter as Router }from 'react-router-dom';
-import Sidebar from './components/sidebar';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./components/sidebar";
+import Banner from "./components/Banner";
+import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Sidebar />
-    </div>
+      <div className="App">
+        <Banner />
+        <Sidebar />
+        <Row
+          title="EROS NOW"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
+        />
+      </div>
     </Router>
   );
 }
